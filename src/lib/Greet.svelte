6691757f9b287fb9927b75fc72f6d09ab8dae5c9
nsdeschenes <nsdeschenes@outlook.com>
2023-04-29 19:51:24 -0300

    setup tailwindcss

--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -9,8 +9,12 @@
   }
 </script>
 
-<div>
-  <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-  <button on:click={greet}>Greet</button>
-  <p>{greetMsg}</p>
+<div class="pl-2">
+  <div class="flex gap-3">
+    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+    <button on:click={greet} class="bg-gray-500 hover:bg-gray-700 py-1 px-2 rounded-md text-white"
+      >Greet</button
+    >
+  </div>
+  <p><span class="font-semibold">Message:</span> {greetMsg}</p>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,5 @@
   import Greet from '../lib/Greet.svelte';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-xl font-semibold">Welcome to SvelteKit</h1>
 <Greet />
