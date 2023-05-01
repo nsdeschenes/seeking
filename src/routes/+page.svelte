92ab380e12790c4e9ec53c2ed27173da01998343
nsdeschenes <nsdeschenes@outlook.com>
2023-05-01 16:51:56 -0300

    basic layout sorted

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,17 @@
 <script>
-  import Greet from '../lib/Greet.svelte';
+  import Request from '../components/Request.svelte';
+  import Response from '../components/Response.svelte';
+
+  let url = '';
 </script>
 
-<h1 class="text-xl font-semibold">Welcome to SvelteKit</h1>
-<Greet />
-<button
-  class="bg-gray-500 hover:bg-gray-700 py-1 px-2 rounded-md text-white"
-  type="button"
-  on:click={() => {
-    throw new Error('Sentry Frontend Error');
-  }}
->
-  Throw error
-</button>
+<div class="flex h-full flex-col gap-2 text-white">
+  <div class="flex justify-center border border-blue-500 px-4 py-3">
+    <input class="border border-red-500" type="text" placeholder="Enter URL" bind:value={url} />
+    <button class="border border-red-500">Request</button>
+  </div>
+  <div class="flex h-full gap-2">
+    <Request />
+    <Response />
+  </div>
+</div>
