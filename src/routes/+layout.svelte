03b1230756ca8ee4ede92149af31ff6b5cdd8b28
nsdeschenes <nsdeschenes@outlook.com>
2023-05-02 21:34:07 -0300

    update base layout for child routes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,18 @@
 <script>
   import '../app.css';
+  import Request from '../components/Request/Request.svelte';
+  import Response from '../components/Response/Response.svelte';
+  import TopBar from '../components/TopBar/TopBar.svelte';
 </script>
 
-<slot />
+<div class="flex h-full flex-col gap-2">
+  <TopBar />
+  <div class="flex h-full flex-col gap-2 text-white">
+    <div class="grid h-full grid-cols-2 gap-2">
+      <Request>
+        <slot />
+      </Request>
+      <Response />
+    </div>
+  </div>
+</div>
