2524007c8535e5268bd7ca6edda8013f3e485696
nsdeschenes <nsdeschenes@outlook.com>
2023-05-02 21:34:50 -0300

    create base routes for different sections nested routing ftw

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,1 @@
-<script>
-  import Request from '../components/Request.svelte';
-  import Response from '../components/Response.svelte';
-
-  let url = '';
-</script>
-
-<div class="flex h-full flex-col gap-2 text-white">
-  <div class="flex justify-center border border-blue-500 px-4 py-3">
-    <input class="border border-red-500" type="text" placeholder="Enter URL" bind:value={url} />
-    <button class="border border-red-500">Request</button>
-  </div>
-  <div class="flex h-full gap-2">
-    <Request />
-    <Response />
-  </div>
-</div>
+<p>body</p>
