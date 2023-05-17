c27b01357efd9fdf26ca724caed26113514bae12
nsdeschenes <nsdeschenes@outlook.com>
2023-05-17 07:38:18 -0300

    create component with debug information

---
+++ src/components/Debug/Debug.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { options } from '@/stores';
+  $: url = $options.url;
+  $: method = $options.method;
+  $: headers = $options.headers;
+</script>
+
+<p>body:</p>
+<p>url: {url}</p>
+<p>method: {method}</p>
