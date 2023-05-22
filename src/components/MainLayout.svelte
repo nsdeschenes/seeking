fc2ea570aa682aafcfe24f57186922752c500d4e
nsdeschenes <nsdeschenes@outlook.com>
2023-05-22 13:57:46 -0300

    remove links to url params and query string links

--- src/components/MainLayout.svelte
+++ src/components/MainLayout.svelte
@@ -21,9 +21,39 @@
   });
 </script>
 
-<TopBar {makeRequest} />
-<div class="flex h-full flex-col gap-2 text-white">
-  <div class="grid h-full grid-cols-2 gap-2">
+<div class="flex h-full flex-col gap-2">
+  <TopBar {makeRequest} />
+  <div class="grid  grid-cols-2 gap-2 pt-2">
+    <ul class="isolate inline-flex gap-2 py-2 justify-center">
+      <li>
+        <a
+          href="/"
+          class="rounded bg-zinc-700 px-4 py-2 font-mono shadow-md hover:cursor-pointer hover:bg-zinc-500 active:bg-zinc-600"
+          >Body</a
+        >
+      </li>
+      <li>
+        <a
+          href="/headers"
+          class="rounded bg-zinc-700 px-4 py-2 font-mono shadow-md hover:cursor-pointer hover:bg-zinc-500 active:bg-zinc-600"
+          >Headers</a
+        >
+      </li>
+      <!-- <li>
+        <a
+          href="/url-params"
+          class="rounded bg-zinc-700 px-4 py-2 font-mono shadow-md hover:cursor-pointer hover:bg-zinc-500 active:bg-zinc-600"
+          >URL Params</a
+        >
+      </li>
+      <li>
+        <a
+          href="/query-string"
+          class="rounded bg-zinc-700 px-4 py-2 font-mono shadow-md hover:cursor-pointer hover:bg-zinc-500 active:bg-zinc-600"
+          >Query String</a
+        >
+      </li> -->
+    </ul>
     <Request>
       <slot />
     </Request>
